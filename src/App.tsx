8ce464d478ed4e47e8c3adda186aa15bf5734495
Tiina Kondio <tiina.kondio@gmail.com>
2022-10-21 13:41:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          TERE TULEMAST<code>sussipoodi.</code> OSTA KOHE!
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
