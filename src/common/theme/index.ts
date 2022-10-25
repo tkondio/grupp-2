b6ee3fd015291da859214a451dc10f5dc2dea0ad
Tiina Kondio <tiina.kondio@gmail.com>
2022-10-25 09:23:43 +0300

    Added theme.

---
+++ src/common/theme/index.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    gradient:
+      "linear-gradient(180deg, #515C4C 0%, rgba(106, 114, 102, 0.735313) 38.54%, rgba(154, 156, 153, 0.23) 100%);",
+    white: "#ffffff",
+    darkGreen: "#324327",
+    middleGreen: "#6D795E",
+    lightGreen: "#6C7E65",
+  },
+};
+
+export default theme;
