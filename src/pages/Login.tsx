7350b36268f9f20a5656efe61d14a181153900e3
Tiina Kondio <tiina.kondio@gmail.com>
2022-10-25 14:59:23 +0300

    Login and register app

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Loginapp } from "../components/Loginapp";
+import { Registerapp } from "../components/Registerapp";
 
 const Login: React.FC = () => {
-  return <div>Siin saad sisse logida.</div>;
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (formName: React.SetStateAction<string>) => {
+    setCurrentForm(formName);
+  };
+
+  return (
+    <div>
+      {currentForm === "login" ? (
+        <Loginapp onFormSwitch={toggleForm} />
+      ) : (
+        <Registerapp onFormSwitch={toggleForm} />
+      )}
+    </div>
+  );
 };
 
 export default Login;
