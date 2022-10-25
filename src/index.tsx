f08beae670febd7e0e7f25563f6fa40025264b9c
Tiina Kondio <tiina.kondio@gmail.com>
2022-10-25 09:38:08 +0300

    index jälle

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationPath from "./routes/NavigationPath";
-import Navbar from "./Components/Navbar";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
