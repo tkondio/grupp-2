43dcc50a964ce8b4d12bb439290f0da80a9d8442
Tiina Kondio <tiina.kondio@gmail.com>
2022-10-26 19:30:14 +0300

    New additions to landingpage.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { flexbox } from "@mui/system";
 import React from "react";
 import { createUseStyles } from "react-jss";
 import { Link } from "react-router-dom";
@@ -5,38 +6,59 @@ import theme from "../common/theme";
 
 const useStyles = createUseStyles({
   catchphrase: {
-    marginLeft: 120,
-    paddingTop: 320,
-    fontSize: 40,
+    //marginLeft: 120,
+    paddingTop: 220,
+    fontSize: 30,
     color: "white",
+    //position: "absolute",
+    width: "455",
+    height: "134",
+    left: "178",
+    top: "425",
+    position: "relative",
+    //fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontWeight: 300,
+    //lineHeight: 67,
+    textAlign: "left",
+    letterSpacing: "-0.075em",
   },
   landingLinks: {
-    marginTop: 120,
-    marginLeft: 220,
+    //marginTop: 120,
+    //marginLeft: 220,
     display: "flex",
     flexDirection: "column",
+    textAlign: "center",
+    alignItems: "center",
+    alignContent: "flex-end",
+    position: "relative",
   },
   landingLink: {
     padding: 20,
-    textDecoration: "none", 
+    textDecoration: "none",
     color: theme.colors.white,
     fontSize: 40,
   },
   linkSeparator: {
     borderColor: theme.colors.lightGreen,
-    borderTop: "1px solid", 
-    marginLeft: 0, 
-    marginRight: 0, 
+    borderTop: "1px solid",
+    //marginLeft: 0,
+    //marginRight: 0,
     width: 180,
   },
   background: {
+    padding: 50,
+    display: "flex",
+    flexDirection: "column",
+    gap: 100,
     backgroundPosition: theme.backgroundImage.backgroundPosition,
     backgroundSize: theme.backgroundImage.backgroundSize,
     backgroundRepeat: theme.backgroundImage.backgroundRepeat,
-    width: theme.backgroundImage.width,
+    //width: theme.backgroundImage.width,
     height: theme.backgroundImage.width,
-    backgroundImage: `url(${"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-three-females-in-slippers-royalty-free-image-1659569918.jpg"})`,
-
+    backgroundAttachment: theme.backgroundImage.backgroundAttachment,
+    alignContent: "center",
+    alignItems: "center",
   },
 });
 
@@ -45,18 +67,22 @@ const LandingPage: React.FC = () => {
 
   return (
     <div
-      className={classes.background} >
-      <div className={classes.catchphrase}>Sinu susside kodu.</div>
+      className={classes.background}
+      style={{ backgroundImage: "url(images/background_landingpage.png)" }}
+    >
+      <div className={classes.catchphrase}>
+        Enam ei pea kodus paljajalu käima.
+      </div>
 
       <div className={classes.landingLinks}>
-          <Link to="./Shop" className={classes.landingLink}>
-            MEHED
-          </Link>
-          <div className={classes.linkSeparator} ></div>
+        <Link to="./Shop" className={classes.landingLink}>
+          MEHED
+        </Link>
+        <div className={classes.linkSeparator}></div>
 
-          <Link to="./Shop" className={classes.landingLink}>
-            NAISED
-          </Link>
+        <Link to="./Shop" className={classes.landingLink}>
+          NAISED
+        </Link>
       </div>
     </div>
   );
