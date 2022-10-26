9991919fd396a10434a24c8c90bcc9ba349613aa
Kaarel Truu <kaareltruu@yahoo.co.uk>
2022-10-26 09:31:21 +0300

    Content added to account page.

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
+import theme from "../common/theme";
+
+// sidepanel
+// recommended
+// toShop
+const useStyles = createUseStyles({
+  background: {
+    display: "flex",
+    backgroundPosition: theme.backgroundImage.backgroundPosition,
+    backgroundSize: theme.backgroundImage.backgroundSize,
+    backgroundRepeat: theme.backgroundImage.backgroundRepeat,
+    backgroundImage:
+      "url(https://images.pexels.com/photos/2909067/pexels-photo-2909067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+  },
+  sidepanel: {
+    display: "flex",
+    flexDirection: "column",
+    width: 300,
+    height: "100vh",
+    backgroundColor: theme.colors.lightGreen,
+    opacity: 0.8,
+  },
+  userInfo: {
+    color: "white",
+    padding: 20,
+  },
+  productArea: {
+    distplay: "flex",
+    padding:20,
+    color: theme.colors.white,
+  },
+  toShop: {
+    display: "flex",
+    alignSelf: "flex-end",
+    marginBottom: 200,
+    marginleft: 700,
+    color: theme.colors.white,
+  }
+});
 
 const Account: React.FC = () => {
-  return <div>Siin lehel näed oma konto infot.</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.background}>
+      <div className={classes.sidepanel}>
+        <div className={classes.userInfo}>Tere kasutaja!
+        <div>Nimi:</div>
+        <div>email:</div>
+        <div>Aadress:</div>
+        <div>Telefon:</div>
+        </div>
+      </div>
+      <div className={classes.productArea}>Soovitame sulle...
+      </div>
+      <div className={classes.toShop}>liigu /selle lingi kaudu/ poodi, et avastada veel susse!</div>
+
+    </div>
+  );
 };
 
 export default Account;
