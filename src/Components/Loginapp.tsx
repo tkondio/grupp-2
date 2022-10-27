108c45db491dda281dba695e2d9fdd2bf2708001
AleksandraKryzh <aleksandra.kryzhanovskaya@gmail.com>
2022-10-27 16:43:17 +0300

    LogIn and Register full make-up, added font/index

--- src/Components/Loginapp.tsx
+++ src/Components/Loginapp.tsx
@@ -2,62 +2,72 @@ import React, { useState } from "react";
 import { createUseStyles } from "react-jss";
 import theme from "../common/theme";
 import { Link } from "react-router-dom";
-
 const useStyles = createUseStyles({
   container: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "space-between",
-    gap: 50, //ei saa aru, miks ta ulatub vaid Logi Sisse ja Rega ennast tekstile
+    gap: 10,
   },
   loginform: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    gap: 50,
+    gap: 24,
   },
-
   inputField: {
     boxSizing: "border-box",
-    width: "632px",
-    height: "95px",
+    width: "451px",
+    height: "53px",
     background: "rgba(255, 255, 255, 0.67)",
     borderbottom: "1px solid #000000",
+    "&::placeholder": {
+      fontSize: "20px",
+      fontFamily: "Montserrat",
+      fontStyle: "normal",
+      fontWeight: "300",
+      color: "#324327",
+    },
   },
-
+  forgotPassword: {
+    fontFamily: "Montserrat",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: "20px",
+    color: "#FFFFFF",
+    lineheight: "108.02%",
+    alignItems: "center",
+  },
+  logInButton: {
+    fontFamily: "Montserrat",
+    fontStyle: "bold",
+    fontWeight: "600",
+    fontSize: "32px",
+    lineHeight: "108.02%",
+    alignItems: "center",
+    letterSpacing: "-0.02em",
+    color: "#FFFFFF",
+    textDecoration: "none",
+    "&:hover": {
+      color: "#324327",
+    },
+  },
   letsRegister: {
-    position: "center",
     width: "446px",
     height: "43px",
-    left: "506px",
-    top: "863px",
-    fontFamily: "Khula",
-    fontStyle: "normal",
-    fontWeight: "300",
-    fontSize: "40px",
+    fontFamily: "Montserrat",
+    fontStyle: "bold",
+    fontWeight: "450",
+    fontSize: "24px",
     lineHeight: "108.02%",
-    display: "flex",
-    alignItems: "center",
-    letterSpacing: "-0.065em",
+    letterSpacing: "-0.02em",
     color: "#FFFFFF",
-  },
-
-  logInButton: {
-    position: "center",
-    width: "169px",
-    height: "43px",
-    left: "666px",
-    top: "751px",
-    fontFamily: "Khula",
-    fontStyle: "normal",
-    fontWeight: "300",
-    fontSize: "40px",
-    lineHeight: "108.02%",
-    display: "flex",
-    alignItems: "center",
-    letterSpacing: "-0.065em",
-    color: "#FFFFFF",
+    background: "none",
+    border: "none",
+    "&:hover": {
+      fontWeight: "600",
+    },
   },
   background: {
     backgroundPosition: theme.backgroundImage.backgroundPosition,
@@ -67,17 +77,14 @@ const useStyles = createUseStyles({
     height: theme.backgroundImage.width,
   },
 });
-
 export const Loginapp = (props: { onFormSwitch: (arg0: string) => void }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const classes = useStyles();
-
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     console.log(email);
   };
-
   return (
     <div className={classes.container}>
       <form className={classes.loginform} onSubmit={handleSubmit}>
@@ -104,17 +111,22 @@ export const Loginapp = (props: { onFormSwitch: (arg0: string) => void }) => {
             className={classes.inputField}
           />{" "}
         </div>
-
+        <div>
+          {" "}
+          <label className={classes.forgotPassword}>
+            {" "}
+            Unustasid parooli? Vajuta <b>SIIA</b>
+          </label>
+        </div>
         <Link to="/shoppingbag" className={classes.logInButton}>
-          Logi sisse
+          LOGI SISSE
         </Link>
-      </form>
-
+      </form>     
       <button
         className={classes.letsRegister}
         onClick={() => props.onFormSwitch("register")}
       >
-        Ei ole kasutajat? Registreeru siin
+        Ei ole kasutajat? Registreeri siin
       </button>
     </div>
   );
