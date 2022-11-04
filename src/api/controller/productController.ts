bba0cd1d588bb5b2d89aa8517b9ea4a086ce9dfd
Valeria Murakas <valerie.valtari@gmail.com>
2022-11-04 09:53:02 +0200

    background added

--- src/api/controller/productController.ts
+++ src/api/controller/productController.ts
@@ -44,4 +44,21 @@ export async function addCartItem(cartItem: CartItemRequestType): Promise<any> {
   return {
     isSuccess: false,
   };
-}
+};
+
+export async function deleteCartItem(cartItem: CartItemRequestType): Promise<any> {
+  const response = await fetch(getPath(ApiPath.Product.cartItem), {
+    method: "DELETE",
+  });
+  if (response.status === 200) {
+    const data = await response.json();
+
+    return {
+      body: data,
+      isSuccess: true,
+    };
+  }
+  return {
+    isSuccess: false,
+  };
+};
